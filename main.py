d058b984dcd7645e9676b7c237405316294b9082
zdx <zzz@qq>
2024-06-28 15:08:17 +0800

    增加背景模糊 模糊等级:1级

--- main.py
+++ main.py
@@ -167,9 +167,36 @@ def make_video_with_srt():
     draft.save()
 
 
+def make_video_with_background_blur():
+    """
+    添加字幕
+    @return:
+    """
+    # 0. 可以给草稿指定名字，比如Draft("古诗词欣赏")；如果不指定名字的话，默认是根据时间戳自动生成草稿名字
+    draft = Draft()
+    root_path = ProjectHelper.get_root_physical_path()
+    middle_path = ".res"
+
+    # 1. 添加两幅图片，分别设置不同的播放时间
+    image1_full_name = os.path.join(root_path, middle_path, "古诗1.jpg")
+    image2_full_name = os.path.join(root_path, middle_path, "古诗2.jpg")
+
+    draft.add_media(image1_full_name)  # 图片如果不指定duration，默认播放5秒
+    draft.add_media(image2_full_name, duration=4_000_000, background_fill_type="模糊")  # 指定图片播放时长为4秒
+
+    # 2. 添加背景音乐，音频长度会根据视频的长度自动剪截
+    music_full_name = os.path.join(root_path, middle_path, "似是故人来.mp3")
+    ## 2.1. 最简方式:直接添加音频文件的地址即可。
+    # draft.add_media(music_full_name)
+    ## 2.2. 也可以给音频指定淡入淡出时长
+    draft.add_media(music_full_name, fade_in_duration=1_000_000, fade_out_duration=1_500_000)
+
+    draft.save()
+
+
 if __name__ == '__main__':
     ## 1. 最简方式（将图片做成视频）
-    basic_using()
+    # basic_using()
 
     ## 2.1 将既有视频进一步处理为新视频
     # make_videos()
@@ -191,3 +218,6 @@ if __name__ == '__main__':
 
     ## 6. 新建带有srt字幕的图片素材
     # make_video_with_srt()
+
+    ## 7. 新建带有背景模糊的图片素材
+    make_video_with_background_blur()
